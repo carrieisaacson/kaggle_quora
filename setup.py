a9f6a8896b58ab5395dbc5b0dececd07e95fb969
Carrie Isaacson <carrie.e.isaacson@gmail.com>
2018-12-15 08:18:48 -0800

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='kaggle competition https://www.kaggle.com/c/quora-insincere-questions-classification',
+    author='carrie isaacson',
+    license='MIT',
+)
